f3ed79da48dee81d49b0864acf5a30250df805bd
Dylan Dislers <dislersd@gmail.com>
2020-09-04 11:38:13 -0700

    created todo list with completed toggling

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom";
 
 const counter = (state = 0, action) => {
@@ -12,6 +12,33 @@ const counter = (state = 0, action) => {
       return state;
   }
 };
+
+const todos = (state = [], action) => {
+  switch (action.type) {
+    case "ADD_TODO":
+      return [
+        ...state,
+        {
+          id: action.payload.id,
+          text: action.payload.text,
+          completed: false,
+        },
+      ];
+    case "TOGGLE_TODO":
+      return state.map((todo) => {
+        if (todo.id !== action.payload) {
+          return todo;
+        }
+        return {
+          ...todo,
+          completed: !todo.completed,
+        };
+      });
+    default:
+      return state;
+  }
+};
+
 const createStore = (reducer) => {
   let state;
   let listeners = [];
@@ -34,7 +61,48 @@ const createStore = (reducer) => {
 
   return { getState, dispatch, subscribe };
 };
-const store = createStore(counter);
+
+const store = createStore(todos);
+
+const Todos = ({ todos, addTodo, toggleTodo }) => {
+  const [todo, setTodo] = useState("");
+  const [id, setId] = useState(0);
+
+  function handleSubmit() {
+    let newTodo = {
+      id,
+      text: todo,
+    };
+
+    setId((oldId) => oldId + 1);
+
+    addTodo(newTodo);
+  }
+
+  return (
+    <div>
+      <input
+        type="text"
+        value={todo}
+        onChange={(e) => setTodo(e.target.value)}
+      />
+      <button onClick={handleSubmit}> Add Todo </button>
+      <ul>
+        {todos.map((todo, i) => (
+          <li
+            key={todo.id}
+            onClick={() => {
+              toggleTodo(todo.id);
+            }}
+            style={{ textDecoration: todo.completed ? "line-through" : "none" }}
+          >
+            {todo.text}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
 const Counter = ({ value, onIncrement, onDecrement }) => (
   <div>
@@ -44,15 +112,27 @@ const Counter = ({ value, onIncrement, onDecrement }) => (
   </div>
 );
 
-const render = () => {
-  ReactDOM.render(
-    <Counter
-      value={store.getState()}
-      onIncrement={() => store.dispatch({ type: "INCREMENT" })}
-      onDecrement={() => store.dispatch({ type: "DECREMENT" })}
-    />,
-    document.getElementById("root")
+const App = () => {
+  return (
+    <>
+      <Todos
+        todos={store.getState()}
+        addTodo={(todo) => store.dispatch({ type: "ADD_TODO", payload: todo })}
+        toggleTodo={(id) =>
+          store.dispatch({ type: "TOGGLE_TODO", payload: id })
+        }
+      />
+      {/* <Counter
+        value={store.getState()}
+        onIncrement={() => store.dispatch({ type: "INCREMENT" })}
+        onDecrement={() => store.dispatch({ type: "DECREMENT" })}
+      /> */}
+    </>
   );
+};
+
+const render = () => {
+  ReactDOM.render(<App />, document.getElementById("root"));
 };
 
 store.subscribe(render);
