9db22b2451a48ba9627ccdca95f6d3c469ab48a1
Dylan Dislers <dislersd@gmail.com>
2020-09-09 15:38:41 -0700

    fixed issue causing app to break on refresh by updating webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,12 @@
 const HtmlWebPackPlugin = require("html-webpack-plugin");
+var path = require("path");
 module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "index_bundle.js",
+    publicPath: "/",
+  },
   module: {
     rules: [
       {
@@ -18,6 +25,9 @@ module.exports = {
         ],
       },
     ],
+  },
+  devServer: {
+    historyApiFallback: true,
   },
   plugins: [
     new HtmlWebPackPlugin({
