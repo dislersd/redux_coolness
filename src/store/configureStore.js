24344cedd508423d9bf6956aebc93c6056caee85
Dylan Dislers <dislersd@gmail.com>
2020-09-09 21:37:04 -0700

    added logging when dispatching actions

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -4,10 +4,26 @@ import { loadState, saveState } from "../Utils/localStorage";
 // throttle ensures our save to local storage function only runs once per second
 import throttle from "lodash/throttle";
 
+const addLoggingToDispatch = (store) => {
+  const rawDispatch = store.dispatch;
+  return (action) => {
+    console.group(action.type);
+    console.log("%c prev state", "color: gray", store.getState());
+    console.log("%c action", "color: red", action);
+    const returnValue = rawDispatch(action);
+    console.log("%c next state", "color: green", store.getState());
+    console.groupEnd(action.type);
+    return returnValue;
+  };
+};
+
 const configureStore = () => {
   const persistedState = loadState();
   const store = createStore(todoApp, persistedState);
   console.log(store.getState());
+  if (process.env.NODE_ENV !== "production") {
+    store.dispatch = addLoggingToDispatch(store);
+  }
 
   store.subscribe(
     throttle(() => {
